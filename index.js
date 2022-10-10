7a494c5163b06ef752e6006ab4a0b9b582887d5a
Krins <krinskumar@gmail.com>
2022-10-10 15:26:34 -0400

    basicjs

--- index.js
+++ index.js
@@ -1,96 +1,73 @@
 // 1 = rock
 // 2 = paper
 // 3 = scissors
-// let options = [`rock`, `paper`, `scissors
 
 let computerInventory = [];
-let userInventory = []
+let userInventory = [];
 let defaultInventory = [`R`,`R`,`R`,`P`,`P`,`P`,`S`,`S`,`S`];
-let chooseTemplate = [`1`,`2`,`3`,`4`,`5`,`6`,`7`,`8`,`9`];
+let full = [`R`, `P`, `S`];
+let half = [`r`, `p`, `s`];
 
-function generatesInventory(rng = 1) {    // Have not implemented the rng part yet
+//---------PreGame----------------------------------------------------------------------------------------------
+function preGame() {    // before the game is started
+    generatesInventory();    // Fills up the inventory for both of the players
+}
+function generatesInventory() {    // sets the inventory for both of the players for the game to start
     computerInventory = defaultInventory;
     userInventory = defaultInventory;
 }
+//---------PreGame----------------------------------------------------------------------------------------------
 
+
+//---------Game-------------------------------------------------------------------------------------------------
 function computerRandom() {    // generates a random number that representates computers hand
-    let random;
-    do {
-        random = Math.ceil(Math.random()*9);
-    } while (computerInventory[random] == '');
-    return random - 1;
-}
+    let random = Math.floor(Math.random()*9); 
 
-function roundWinner(userHand, computerHand = computerRandom()) {    // decides the winner of the round
-
-   
+    let direction = Math.floor(Math.random()*2);    // to get a proper hand for the computer
+    if (direction === 0) {direction = -1;}
+    while (computerInventory[random] === '*') {
+        random += direction
+    }
+    return random;
 }
 
 function checkLives(array) {    // checks the inventory of both players to get the winner
-    livesLeft = array.filter(checkLost);
+    livesLeft = array.filter(function() {
+        if (tools === `*`) {return false;}
+        return true;
+    });
     return livesLeft.length;
 }
 
-function checkLost(tools) {    // Checks the passed index of inventory 
-    if (tools === `*`) {
-        return false;
+function roundWinner(userIndex, computerIndex) {    // decides the winner of the round
+    let userHand = full.indexOf(userInventory[userIndex]);
+    let computerHand = full.indexOf(computerInventory[computerIndex]);
+}
+
+
+function gameround(userIndex) {
+    let computerIndex = computerRandom()
+    let result = roundWinner(userIndex, computerIndex);
+
+    if (result === true) {    // if user wins 
+        let userHand = full.indexOf(userInventory[userIndex]);
+        if (userHand) {userInventory[userIndex] = half[userHand];}    // updates users inventory
+        else {userInventory[userIndex] = `*`};
+        computerInventory[computerIndex] = `*`;    // updates computers inventory
+    } 
+    else if (result === false) {    // if computer wins 
+        userInventory[computerIndex] = `*`;    // updates users inventory
+        let computerHand = full.indexOf(computerInventory[computerIndex]);
+        if (computerHand) {computerInventory[computerHand] = half[computerHand];}    // updates computers inventory
+        else {computerInventory[computerHand] = `*`};
+    } 
+    else {    // if theres a tie
+        let userHand = full.indexOf(userInventory[userIndex]);
+        let computerHand = full.indexOf(computerInventory[computerIndex]);
+        if (userHand) {userInventory[userIndex] = half[userHand];}    // updates users inventory
+        else {userInventory[userIndex] = `*`};
+        if (computerHand) {computerInventory[computerHand] = half[computerHand];}    // updates computers inventory
+        else {computerInventory[computerHand] = `*`};
     }
-     return true;
 }
-
-function startGame() {
-    generatesInventory();  // Fills up the inventory for both of the players
-
-    do {
-        console.log(`AI hasss ` + computerInventory);
-        console.log(`You have ` + userInventory);
-        console.log(`Choooose ` + chooseTemplate);
-
-        
-
-    } while (checkLives(computerInventory) > 0 || checkLives(userInventory) > 0);
- }
-
- function gameMenu(){
-    
-    let selection = 0;
- 
-    console.log(
-`        █▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█
-         █░░ ROCK-PAPER-SCISSORS ░░█
-         █▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█
-         █▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█
-         █░░        MENU         ░░█
-         █▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█
-         █▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█
-         █░░        MENU         ░░█
-         █░░---------------------- █
-         █░░ 1. Start Game       ░░█
-         █░░ 2. Rules            ░░█
-         █░░ 3. History          ░░█
-         █░░ 4. Quit             ░░█
-         █▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█`);
-    do {
-       selection = Object.values(process.argv).slice(2).join(' ').toString();
-         switch(selection) {
-            case 1:
-                rules();
-                break;
-            case 2:
-                rules();
-                break;
-            case 3:
-                history();
-                break;
-            case 4:
-                console.log(`Thankyou for playing my game!`);
-                break;
-            default:
-                console.log(`uh-Oh Wrong Input, try again.`);
-         }
-
-    } while (selection != 4);
-}
-
- // Function call to start the game
- gameMenu();
+//---------Game-------------------------------------------------------------------------------------------------
