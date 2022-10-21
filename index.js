4cee27e7ff17b37140ef150f701df638f7143f52
Krinskumar Vaghasia <krinskumar@gmail.com>
2022-10-21 14:39:41 -0400

    adjusted-the-game-rules

--- index.js
+++ index.js
@@ -72,22 +72,18 @@ function gameround(userIndex) {    // simulates a round
     if (result === true) {    // if user wins 
         let userHand = full.indexOf(userInventory[userIndex]);
         if (userHand != -1) {userInventory[userIndex] = half[userHand];}    // updates users inventory
-        else {userInventory[userIndex] = `*`};
         computerInventory[computerIndex] = `*`;    // updates computers inventory
     } 
     else if (result === false) {    // if computer wins 
         userInventory[userIndex] = `*`;    // updates users inventory
         let computerHand = full.indexOf(computerInventory[computerIndex]);
         if (computerHand != -1) {computerInventory[computerIndex] = half[computerHand];}    // updates computers inventory
-        else {computerInventory[computerIndex] = `*`};
     } 
-    else {    // if theres a tie
+    else {    // if theres a tie - cannot loose a life if there is a tie.
         let userHand = full.indexOf(userInventory[userIndex]);
         let computerHand = full.indexOf(computerInventory[computerIndex]);
-        if (userHand != -1) {userInventory[userIndex] = half[userHand];}    // updates users inventory
-        else {userInventory[userIndex] = `*`};
-        if (computerHand != -1) {computerInventory[computerIndex] = half[computerHand];}    // updates computers inventory
-        else {computerInventory[computerIndex] = `*`};
+        if (userHand != -1) {userInventory[userIndex] = half[userHand];}    
+        if (computerHand != -1) {computerInventory[computerIndex] = half[computerHand];}    
     }
 }
 
@@ -108,6 +104,12 @@ function displayInventory() {
 }
 
 
-//---------DOM--------------------------------------------------------------------------------------------------
-
+//Documentation:
+/*
+    User and computer has 3 of all that is rock paper and scissors. 
+    When a piece from inventory is selected computer selects a piece randomly 
+    If the user wins the durability of the selected item gets half and if the durability is already half, nothing happens
+    If the user loosese, He looses the selected item
+    If there is a tie both of the selected item looses the durability and if the durablity is already half nothing happens
+*/ 
 
